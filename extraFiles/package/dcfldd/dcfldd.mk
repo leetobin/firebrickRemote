e825e194d09752b32db3c8d5a9b8d0e912030df9
Lee Tobin <leetobin@gmail.com>
2017-02-28 13:48:40 +0000

    Update dcfldd.mk
    Broken download for 1.3.4-1 so we'll use 1.3.4 for now.

--- extraFiles/package/dcfldd/dcfldd.mk
+++ extraFiles/package/dcfldd/dcfldd.mk
@@ -1,5 +1,5 @@
-DCFLDD_VERSION=1.3.4-1
-DCFLDD_SOURCE = dcfldd-1.3.4-1.tar.gz
+DCFLDD_VERSION=1.3.4
+DCFLDD_SOURCE = dcfldd-1.3.4.tar.gz
 DCFLDD_SITE = http://prdownloads.sourceforge.net/dcfldd
 DCFLDD_AUTORECONF = NO
 DCFLDD_INSTALL_STAGING = NO
